24610fa9f822e2ab7e0b684251ac327bce7f2820
Overshooting <overshoot69@gmail.com>
2025-08-30 12:04:59 -0700

    Finished Lab1

---
+++ Labs/Lab1/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * This is my first java app. It prints out Hello World and my name to the console
+ * @author Alexander Melis, aamelis@gmail.com
+ * @since 8/30/2025
+ * @version 1.0
+ */
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Alexander Melis");
+    }
+
+}
